2cf1940f1afe1c23c593a2d0968ef93d94770f08
Kelly Dwan <ryelle@users.noreply.github.com>
2024-10-17 14:38:33 -0400

    Update archive display initial setup

--- env/setup.sh
+++ env/setup.sh
@@ -6,7 +6,7 @@ wp theme activate wporg-photos-2024
 
 wp option update blogname "Photos"
 wp option update blogdescription "Choose from a growing collection of free, CC0-licensed photos to customize and enhance your WordPress website."
-wp option update posts_per_page 18
+wp option update posts_per_page 24
 
 wp import "${root}/env/data/wporg-photos-pages.xml" --authors=create
 wp import "${root}/env/data/wporg-photos-photos.xml" --authors=create
