d2cbd8094d8c8193bd924259aac2f0d1e690d791
Kelly Dwan <ryelle@users.noreply.github.com>
2024-12-06 10:52:12 -0500

    Filters: Use columns for filter lists
    Fixes #25

--- source/wp-content/themes/wporg-photos-2024/patterns/page-filters.php
+++ source/wp-content/themes/wporg-photos-2024/patterns/page-filters.php
@@ -8,24 +8,40 @@
 ?>
 <!-- wp:post-title {"level":1,"fontSize":"heading-3"} /-->
 
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:heading -->
-	<h2 class="wp-block-heading"><?php esc_html_e( 'Categories', 'wporg-photos' ); ?></h2>
-	<!-- /wp:heading -->
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0">
+	<!-- wp:columns -->
+	<div class="wp-block-columns">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:heading -->
+			<h2 class="wp-block-heading"><?php esc_html_e( 'Categories', 'wporg-photos' ); ?></h2>
+			<!-- /wp:heading -->
 
-	<!-- wp:categories {"taxonomy":"photo_category","showPostCounts":true,"className":"is-tax-photo_categories"} /-->
+			<!-- wp:categories {"taxonomy":"photo_category","showPostCounts":true,"className":"is-tax-photo_categories"} /-->
+		</div>
+		<!-- /wp:column -->
 
-	<!-- wp:heading -->
-	<h2 class="wp-block-heading"><?php esc_html_e( 'Colors', 'wporg-photos' ); ?></h2>
-	<!-- /wp:heading -->
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:heading -->
+			<h2 class="wp-block-heading"><?php esc_html_e( 'Colors', 'wporg-photos' ); ?></h2>
+			<!-- /wp:heading -->
 
-	<!-- wp:categories {"taxonomy":"photo_color","showPostCounts":true,"className":"is-tax-photo_color"} /-->
+			<!-- wp:categories {"taxonomy":"photo_color","showPostCounts":true,"className":"is-tax-photo_color"} /-->
+		</div>
+		<!-- /wp:column -->
 
-	<!-- wp:heading -->
-	<h2 class="wp-block-heading"><?php esc_html_e( 'Orientations', 'wporg-photos' ); ?></h2>
-	<!-- /wp:heading -->
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:heading -->
+			<h2 class="wp-block-heading"><?php esc_html_e( 'Orientations', 'wporg-photos' ); ?></h2>
+			<!-- /wp:heading -->
 
-	<!-- wp:categories {"taxonomy":"photo_orientation","showPostCounts":true,"className":"is-tax-photo_orientation"} /-->
+			<!-- wp:categories {"taxonomy":"photo_orientation","showPostCounts":true,"className":"is-tax-photo_orientation"} /-->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
 </div>
 <!-- /wp:group -->
